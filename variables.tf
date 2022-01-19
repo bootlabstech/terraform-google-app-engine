2da269577ee8864f7d5ea30abe027ad0d00a3831
Karthick Dharman <karthick.dharman@bootlabstech.com>
2022-01-19 22:15:43 +0530

    <No ID> <terraform template standardisation>

--- variables.tf
+++ variables.tf
@@ -1,70 +1,53 @@
 //required variables
 
-variable "project" {
+variable "project_id" {
   description = "The project ID to create the application under."
   type        = string
 }
 
-variable "location_id" {
+variable "location" {
   description = "The location to serve the app from."
   type        = string
 }
-
-variable "split_health_checks" {
-  description = " Set to false to use the legacy health check instead of the readiness and liveness checks."
-  type        = bool
-}
-
-variable "oauth2_client_id" {
-  description = "OAuth2 client ID to use for the authentication flow."
-  type        = string
-}
-
-variable "oauth2_client_secret" {
-  description = "OAuth2 client secret to use for the authentication flow. "
-  type        = string
-
-}
-
-variable "action" {
-  description = "IP address or range, defined using CIDR notation, of requests that this rule applies to."
-  type        = string
-}
-
-variable "source_range" {
-  description = "The action to take if this rule matches."
-  type        = string
-}
-
-//optional variables
 
 
 variable "auth_domain" {
   description = "The domain to authenticate users with when using App Engine's User API."
   type        = string
-  default     = ".com"
+  default     = null
 }
 
 variable "database_type" {
   description = "The type of the Cloud Firestore or Cloud"
   type        = string
-  default     = "CLOUD_DATASTORE"
+  default     = null
+  validation {
+    condition     = var.database_type == null || contains(["CLOUD_FIRESTORE", "CLOUD_DATASTORE_COMPATIBILITY"], var.database_type == null ? "" : var.database_type)
+    error_message = "The database type shall be one of the [CLOUD_FIRESTORE, CLOUD_DATASTORE_COMPATIBILITY]."
+  }
 }
 
 variable "serving_status" {
   description = "The serving status of the app."
   type        = string
-  default     = "SERVING"
+  default     = null
+  validation {
+    condition     = var.serving_status == null || contains(["UNSPECIFIED", "SERVING", "USER_DISABLED", "SYSTEM_DISABLED"], var.serving_status == null ? "" : var.serving_status)
+    error_message = "The serving status shall be one of the [UNSPECIFIED, SERVING, USER_DISABLED, SYSTEM_DISABLED]."
+  }
 }
-
-variable "description" {
-  description = "An optional string description of this rule."
-  type        = string
-  default     = "Rule to App-engine-create"
+variable "feature_settings" {
+  description = "A block of optional settings to configure split health checks"
+  type = object({
+    split_health_checks = bool
+  })
+  default = null
 }
-
-variable "priority" {
-  description = "A positive integer that defines the order of rule evaluation."
-  type        = number
-  default     = 32
+variable "iap" {
+  description = "Settings for enabling Cloud Identity Aware Proxy"
+  type = object({
+    oauth2_client_id     = string,
+    oauth2_client_secret = string
+  })
+  default = null
 }
